a98a000d0e02048d32f34ffc2a37843b415fd061
Keenlyien <ritsunikki@gmail.com>
2026-07-08 08:14:05 +0800

    updated contact

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -23,7 +23,7 @@ const contactLinks = [
   },
   {
     name: 'Email',
-    url: 'mailto:keenlyien@example.com',
+    url: 'https://mail.google.com/mail/?view=cm&fs=1&to=calub.kennethjulius@gmail.com',
     icon: (
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
